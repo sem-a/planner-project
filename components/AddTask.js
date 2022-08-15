af256a815b4d80ba74945cf53728d3b9dfe94386
sem-a <aleksey.i99@mail.ru>
2022-08-15 14:30:03 +0300

    async storage

--- components/AddTask.js
+++ components/AddTask.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, SafeAreaView, Text, TextInput, Button } from 'react-native';
 import { Ionicons, Feather } from '@expo/vector-icons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+
+
+let STORAGE_KEY = '@task_input';
+
 export default function AddTask() {
+  
+  const [taskInput, setTaskInput] = useState('');
+
+  const saveData = async () => {
+    try {
+      await AsyncStorage.setItem(STORAGE_KEY, task)
+      console.log('Data successfully saved')
+    } catch(e) {
+      console.log('Failed to save the data to the storage')
+    }
+  };
+  const readData = async () => {
+    try {
+      const value = await AsyncStorage.getItem(STORAGE_KEY);
+      if (value !== null) {
+        setTaskInput(value);
+      }
+    } catch(e) {
+      console.log('Failed to fetch the input from storage');
+    }
+  };
+  const onChangeText = value => setTaskInput(value);
+  const onSubmitEditing = () => {
+    if (!taskInput) return;
+
+    saveData(taskInput);
+    setTaskInput('');
+  };
+
   
   
   return (
     <View style={styles.container}>
-      <TextInput style={styles.addTaskForm} placeholder='Введите задачу...' />
+      <TextInput style={styles.addTaskForm} placeholder='Введите задачу...' 
+        value={taskInput}
+        onChangeText={onChangeText}
+        onSubmitEditing={onSubmitEditing}
+      />
       <TextInput style={styles.addSubTaskForm} placeholder='Добавить подзадачу' />
+      <Text>{taskInput}</Text>
       <View style={styles.addTaskPanel}>
         <View>
           <Feather name="calendar" size={30} color="#D9D9D9" />
