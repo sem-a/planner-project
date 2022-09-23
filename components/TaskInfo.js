765ebb7a51180191f454aa113542cfb6ee37fcb8
sem-a <aleksey.i99@mail.ru>
2022-09-23 22:31:06 +0300

    новый чекбокс, тоже пока не работает

--- components/TaskInfo.js
+++ components/TaskInfo.js
@@ -4,9 +4,24 @@ import { rewriteTaskStore } from './db';
 import style from '../styles/globalStyle.module.css';
 import Ionicons from '@expo/vector-icons';
 
-
+function MyCheckBox( {checked, onChange} ) {
+  function onCheckmarkPress() {
+    onChange(!checked);
+  }
+  return (
+    <Pressable 
+      style={[style.checkboxBase, checked && style.checkboxChecked]}
+      onPress={onCheckmarkPress}
+    >
+      {checked && <Ionicons name="checkmark" size={24} color="white" />}
+    </Pressable>
+  );
+}
 
 export default function TaskInfo( {item, taskId, addComplete} ) {
+
+  const [checked, onChange] = useState(false);
+
   let taskName;
   if (item != undefined) {
     taskName = item.taskName;
@@ -26,6 +41,10 @@ export default function TaskInfo( {item, taskId, addComplete} ) {
     <View>      
       <View>
         <TouchableOpacity style={style.task__item} onPress={rewriteTaskButton}>
+          <MyCheckBox
+            checked={checked}
+            onChange={onChange}
+          />
           <Text style={style.info__text}>{taskName}</Text>
         </TouchableOpacity>
       </View>
